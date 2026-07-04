0a7daa36b2be6493b375b13a6a4e26926fce4263
fy-sea <mirainohibiki@gmail.com>
2026-07-04 23:40:09 +0900

    Move stories to /posts and call them as .md files instead of .json for easier write up

--- js/storylist.js
+++ js/storylist.js
@@ -5,7 +5,9 @@
   var bookSVG = '<span><svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /></svg></span>';
 
   function buildCard(story) {
-    var href = 'story.html?story=' + story.slug;
+    var href = story.type === 'branching'
+      ? 'story.html?story=' + story.slug
+      : 'reader.html?post=' + story.slug;
     var genres = (story.genres || []).map(function (g) { return '<li>' + g + '</li>'; }).join('');
     return '<div class="story-list-card">'
       + '<a class="story-list-card-img" href="' + href + '" style="background-image: url(' + story.image + ')"></a>'
