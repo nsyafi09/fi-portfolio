b9363f9a0fd65443f280567307aaa0f5ebd431b5
fy-sea <mirainohibiki@gmail.com>
2026-07-05 21:53:21 +0900

    Mega fix

--- js/storylist.js
+++ js/storylist.js
@@ -2,6 +2,14 @@
   var mount = document.getElementById('story-list-mount');
   if (!mount) return;
 
+  function esc(str) {
+    return String(str)
+      .replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;');
+  }
+
   var bookSVG = '<span><svg xmlns="http://www.w3.org/2000/svg" fill="black" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" /></svg></span>';
 
   function buildCard(story) {
@@ -10,11 +18,11 @@
       : 'reader.html?post=' + story.slug;
     var genres = (story.genres || []).map(function (g) { return '<li>' + g + '</li>'; }).join('');
     return '<div class="story-list-card">'
-      + '<a class="story-list-card-img" href="' + href + '" style="background-image: url(' + story.image + ')"></a>'
+      + '<a class="story-list-card-img" href="' + href + '" style="background-image: url(' + esc(story.image) + ')"></a>'
       + '<div class="story-list-right">'
-      + '<div class="story-data-list"><p>' + bookSVG + (story.series || '—') + '</p></div>'
-      + '<div class="story-title-list" data-aos="fade-up"><a href="' + href + '"><h1>' + story.title + '</h1></a></div>'
-      + '<div class="story-preview" data-aos="zoom-in"><a href="' + href + '"><p>' + story.preview + '</p></a></div>'
+      + '<div class="story-data-list"><p>' + bookSVG + esc(story.series || '—') + '</p></div>'
+      + '<div class="story-title-list" data-aos="fade-up"><a href="' + href + '"><h1>' + esc(story.title) + '</h1></a></div>'
+      + '<div class="story-preview" data-aos="zoom-in"><a href="' + href + '"><p>' + esc(story.preview) + '</p></a></div>'
       + '<div class="story-genre"><ul>' + genres + '</ul></div>'
       + '</div>'
       + '</div>';
@@ -26,7 +34,8 @@
       mount.innerHTML = stories.map(buildCard).join('');
       if (window.AOS) AOS.refresh();
     })
-    .catch(function () {
+    .catch(function (err) {
+      console.error(err);
       mount.innerHTML = '<p style="text-align:center;color:#555;padding:2rem;">Could not load stories.</p>';
     });
 })();
